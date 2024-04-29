03bc2908291ef80c235de525d64c0b90eca29812
Oganes <og.marckaryan@yandex.ru>
2024-04-29 14:30:36 +0300

    add slider

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'prod'),
+    filename: '[name]..bundle.js',
+    assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html', // откуда брать шаблон
+    }),
+  ],
+};
