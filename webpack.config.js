b4692221da59d0d1855b34c796340b9d3cf1b709
Oganes <og.marckaryan@yandex.ru>
2024-04-29 18:42:52 +0300

    babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      { 
+        test: /\.(js)$/, 
+        use: 'babel-loader' 
+      },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
         type: 'asset/resource',
